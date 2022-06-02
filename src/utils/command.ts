27d9b2ffce57bd29e2671da09612bc0b69b36831
teav4 <teav4@feadorra.com>
2022-06-02 17:49:15 +0700

    feat: improve search key

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -1,9 +1,11 @@
 import { EMOJI_0, EMOJI_1, EMOJI_2, EMOJI_3, EMOJI_4, EMOJI_5, EMOJI_6, EMOJI_7, EMOJI_8, EMOJI_9 } from "../constants/reply.constants"
+import { isArray } from './validate'
 
 interface CommandInitProps {
-  commandIs(key: string): boolean;
+  commandIs(key: string|string[]): boolean;
   commandStartAt(key: string[]): boolean;
-  getCommandArgs(prefix: string): string[]
+  getCommandArgs(prefix: string): string[];
+  commandIsIncludes(key: string[]): boolean;
 }
 
 function commandStartAt(key: string[], from: string): boolean {
@@ -14,8 +16,18 @@ function commandStartAt(key: string[], from: string): boolean {
   return false
 }
 
-function commandIs(key: string, string: string): boolean {
-  return key.trim() === string.trim()
+function commandIs(key: string|string[], message: string): boolean {
+  if (isArray(key)) {
+    return key.indexOf(message) > -1
+  }
+
+  return key === message.trim()
+}
+
+function commandIsIncludes(key: string[], message: string): boolean {
+  const isInclude = (m: string) => key.some(e => m.includes(e))
+
+  return isInclude(message+' ') || isInclude(' '+message) || commandIs(message, message)
 }
 
 function getCommandArgs(prefix: string, message: string): string[] {
@@ -26,7 +38,8 @@ export function commandInit(message: string): CommandInitProps {
   return {
     commandIs: (key) => commandIs(key, message),
     commandStartAt: (key) => commandStartAt(key, message),
-    getCommandArgs: (prefix) => getCommandArgs(prefix, message)
+    getCommandArgs: (prefix) => getCommandArgs(prefix, message),
+    commandIsIncludes: (key) => commandIsIncludes(key, message)
   }
 }
 
